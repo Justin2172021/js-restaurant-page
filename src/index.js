1ffbf16b3b65b721c801863c6ac9570a58356a24
Justin2172021 <justin.busching@gmail.com>
2022-05-24 17:06:21 -0500

    Added DOM manip for the landing page

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-console.log("This is my console.log statement from /src/index.js HAHA!!");
+import * as module from './initial-page-load.js'
+
+console.log("This is my console.log statement from /src/index.js");
+module.initialPageLoad();
